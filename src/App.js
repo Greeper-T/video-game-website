7efda6be5ba2c547798fc4e933386ac05a01dd59
Greeper-T <hyrtwisback@gmail.com>
2024-10-23 09:07:37 -0500

    more card things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Stack } from 'react-bootstrap';
 import './App.css';
 import VideogameSlider from './components/VideogameSlider';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <Stack direction='horizontal' gap={2} >
+      <VideogameSlider/>
+      <VideogameSlider/>
+      <VideogameSlider/>
+      <VideogameSlider/>
+      <VideogameSlider/>
       <VideogameSlider/>
     </Stack>
   );
