e54d11c19c52af2367a5ae145b911e53db9e10d9
Greeper-T <hyrtwisback@gmail.com>
2024-10-25 08:57:47 -0500

    tailwind addtion

--- src/components/VideogameSlider.js
+++ src/components/VideogameSlider.js
@@ -6,13 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function VideogameSlider(){
     return(
-        <Card style={{width: '10rem'}} variant='primary'>   
-            <Card.Body>
-                <Card.Title>The Binding Of Isaac</Card.Title>
+        <Card className="width:8rem p-2 bg-emerald-400">   
+            <Card.Body className="bg-emerald-400">
+                <Card.Title className="bg-emerald-400">The Binding Of Isaac</Card.Title>
             </Card.Body>
             <Card.Img variant="top" src={tboiImage}/>
-            <Card.Body>
-                <Button variant="secondary" href="https://store.steampowered.com/app/1426300/The_Binding_of_Isaac_Repentance/">go to web </Button>
+            <Card.Body className="bg-emerald-400">
+                <Button variant="secondary" href="https://store.steampowered.com/app/1426300/The_Binding_of_Isaac_Repentance/" target="_blank">go to web </Button>
+                <button className="bg-emerald-400"> nooo </button>
             </Card.Body>
         </Card>
     )
