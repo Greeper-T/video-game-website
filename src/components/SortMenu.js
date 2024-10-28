e2eafa7c8a329fc164df8feac941f3954e107f03
PointyHatCone <kimbryce2007@gmail.com>
2024-10-28 09:03:42 -0500

    Sort Menu

--- src/components/SortMenu.js
+++ src/components/SortMenu.js
@@ -35,11 +35,25 @@ function SortMenu(){
 
       <Dropdown.Menu>
       <FormGroup>
-  <FormControlLabel control={<Checkbox />} label="Nintendo" />
-  <FormControlLabel control={<Checkbox />} label="Play Station" />
-  <FormControlLabel control={<Checkbox />} label="Xbox" />
-  <FormControlLabel control={<Checkbox />} label="PC" />
-  <FormControlLabel control={<Checkbox />} label="Mobile" />
+      <FormControlLabel control={<Checkbox />} label="Action" />
+  <FormControlLabel control={<Checkbox />} label="Indie" />
+  <FormControlLabel control={<Checkbox />} label="Adventure" />
+  <FormControlLabel control={<Checkbox />} label="RPG" />
+  <FormControlLabel control={<Checkbox />} label="Strategy" />
+  <FormControlLabel control={<Checkbox />} label="Shooter" />
+  <FormControlLabel control={<Checkbox />} label="Casual" />
+  <FormControlLabel control={<Checkbox />} label="Simulation" />
+  <FormControlLabel control={<Checkbox />} label="Puzzle" />
+  <FormControlLabel control={<Checkbox />} label="Arcade" />
+  <FormControlLabel control={<Checkbox />} label="Platformer" />
+  <FormControlLabel control={<Checkbox />} label="Racing" />
+  <FormControlLabel control={<Checkbox />} label="Massively Multiplayer" />
+  <FormControlLabel control={<Checkbox />} label="Sports" />
+  <FormControlLabel control={<Checkbox />} label="Fighting" />
+  <FormControlLabel control={<Checkbox />} label="Family" />
+  <FormControlLabel control={<Checkbox />} label="Board Games" />
+  <FormControlLabel control={<Checkbox />} label="Educational" />
+  <FormControlLabel control={<Checkbox />} label="Card" />
 </FormGroup>
       </Dropdown.Menu>
     </Dropdown>
