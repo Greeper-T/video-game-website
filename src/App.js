b51c567ad0406c815a832c1757b16d9d87b3af43
PointyHatCone <kimbryce2007@gmail.com>
2024-10-28 23:34:31 -0500

    Added side bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import VideogameSlider from './components/VideogameSlider';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import SortMenu from './components/SortMenu';
+import SideBarMenu from './components/SideBarMenu';
 
 function App() {
   return (
     <div>
+      <SideBarMenu/>
       <SortMenu/>
       <VideogameSlider/>
     </div>
