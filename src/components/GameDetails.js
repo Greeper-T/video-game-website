04bfb356f8ed6da14b49b8d40ee1b3e280bf1887
Greeper-T <hyrtwisback@gmail.com>
2024-11-07 09:05:24 -0600

    dark mode

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -32,11 +32,11 @@ function GameDetails() {
   return (
 
     <div style={{ padding: '20px', color: 'black' }}>
-      <div className="row">
+      <div className="row dark:bg-gray-900">
       <div className={`col-auto col-md-5 min-vh-75`}>
       <i className="bi bi-house-door"></i>
-        <button className="" onClick={handleClick}>Home</button>
-        <h1>{game.name}</h1>
+        <button className="dark:text-white" onClick={handleClick}>Home</button>
+        <h1 className="dark:text-white">{game.name}</h1>
         <img src={game.background_image} alt={game.name}/>
          </div>
       <div className={`col-auto col-md-1 min-vh-75`}>
@@ -46,22 +46,16 @@ function GameDetails() {
           <br/>
           <br/>
         <strong className="fs-1">Details:</strong>
-      <p><strong>Release Date:</strong> {game.released}</p>
-      <p><strong>Developer:</strong> {game.developers?.map(dev => dev.name).join(', ')}</p>
-      <p><strong>Genres:</strong> {game.genres?.map(genre => genre.name).join(', ')}</p>
-      <p><strong>Platforms:</strong> {game.platforms?.map(platform => platform.platform.name).join(', ')}</p>
+      <p className="dark:text-white"><strong>Release Date:</strong> {game.released}</p>
+      <p className="dark:text-white"><strong>Developer:</strong> {game.developers?.map(dev => dev.name).join(', ')}</p>
+      <p className="dark:text-white"><strong>Genres:</strong> {game.genres?.map(genre => genre.name).join(', ')}</p>
+      <p className="dark:text-white"><strong>Platforms:</strong> {game.platforms?.map(platform => platform.platform.name).join(', ')}</p>
         </div>
         <div className="mt-5">
-      <p><strong>About Game:</strong> {game.description_raw}</p>
-      <h3>Additional Content:</h3>
-      <p><strong>DLCs:</strong> {game.additions?.length > 0 ? game.additions.map(dlc => dlc.name).join(', ') : 'None'}</p>
+      <p className="dark:text-white"><strong>About Game:</strong> {game.description_raw}</p>
+      <h3 className="dark:text-white">Additional Content:</h3>
+      <p className="dark:text-white"><strong>DLCs:</strong> {game.additions?.length > 0 ? game.additions.map(dlc => dlc.name).join(', ') : 'None'}</p>
 
-      <h3>Screenshots:</h3>
-      <div style={{ display: 'flex', gap: '10px' }}>
-        {game.short_screenshots?.map((screenshot, index) => (
-          <img key={index} src={screenshot.image} alt={`Screenshot ${index + 1}`} style={{ width: '30%' }} />
-        ))}
-      </div>
       </div>
       </div>
       
